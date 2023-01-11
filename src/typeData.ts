0119c138dde3141eb6e65ba74285bd5b6212fb6a
Dicky Pratama <dickypratama4792@gmail.com>
2023-01-11 13:07:17 +0700

    learn array data type

---
+++ src/typeData.ts
@@ -0,0 +1,25 @@
+// string
+let firstName: string = "Dicky"
+let lastName: string = "Pratama"
+let fullName: string = `${firstName} ${lastName}`
+fullName = `My name ${fullName}`
+
+// number
+let age: number = 30
+age = 17
+
+// boolean
+let isMarried: boolean = false
+isMarried = true
+
+// any
+let random: any = "Qwerty"
+random = 123
+random = []
+random = {}
+random = true
+
+// union type / mixed
+let phone: number | string;
+phone = "081123377"
+phone = 628123333
