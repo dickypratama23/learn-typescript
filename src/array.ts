3f5a2817ad182ee744c6a7255781e1ddddbcb5f9
Dicky Pratama <dickypratama4792@gmail.com>
2023-01-11 13:07:22 +0700

    learn array data type

---
+++ src/array.ts
@@ -0,0 +1,7 @@
+// array
+let arrNumber: number[] = [1, 2, 3]
+let arrString: string[] = ["Q", "W", "E"]
+let arrAny: any[] = [1, "Q", true]
+
+//tuple
+let arrTuple: [string, number, object, boolean] = ["Q", 1, {}, true]
